12e74656b201919b15589be978f0733fff2ed9b4
Cem AÇAR <myceykiii@gmail.com>
2019-01-08 11:15:20 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php 
+$path='../src';
+$files=scandir($path);
+//print_r($files);
+
+$a=0;
+foreach ($files as $key => $value) { 
+   $dt= $path."/".$value ;
+   $file_parts = pathinfo($dt); 
+   if($file_parts["extension"]=="txt"){ 
+   		 $cs=unlink($dt); 
+   		 if($cs){
+   		 	 $a++;
+   		 
+   }
+    
+}
+
+echo $a;
+?>
